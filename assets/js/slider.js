18f76206950faf73fcc006d35381d7b3f99a56a7
Capelaum <thecapellett@gmail.com>
2020-05-17 17:50:40 -0300

    Init Sections

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -4,6 +4,8 @@ window.onload = function () {
     setInterval(passarSlide, 2000);
 
     var slidewidth = document.getElementById("slideshow").offsetWidth;
+    console.log("window.onload -> slidewidth = ", slidewidth);
+
     var objs = document.getElementsByClassName("slide");
 
     for (var i = 0; i < objs.length; i++) {
@@ -19,6 +21,23 @@ function passarSlide() {
     } else {
         slideItem++;
     }
+
+    //* Change Active Pointers
+    document.querySelectorAll('.sliders-pointers').forEach( (pointer, pointerIndex) => {
+        //console.log('here');
+
+        pointer.classList.remove('active');
+        //console.log(pointer);
+
+
+        pointer.addEventListener( 'click', () => {
+            console.log("passarSlide -> pointerIndex = ", pointerIndex)
+            
+            document.querySelector('.pointer.active').classList.remove('active');
+            pointer.classList.add('active');
+
+        })
+    })
 
     document.getElementsByClassName("sliders")[0].style.marginLeft = "-" + (slidewidth * slideItem) + "px";
 }
